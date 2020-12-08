88a1badf58b396f9aa4ff1dac12ecc6739971ceb
eliasall3x <elliasallex@gmail.com>
2020-12-07 23:54:42 -0300

    feat: ToogleTheme

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Footer from './components/Footer';
-
-// import { Container } from './styles';
+import { ThemeProvider } from 'styled-components'
 
 import Header from './components/Header'
 import Profile from './pages/Profile'
 import Repo from './pages/Repo'
+import Footer from './components/Footer';
 // STATIC
 import GlobalStyles from './styles/GlobalStyles'
+import { ThemeName, themes } from './styles/themes';
 
 const App: React.FC = () => {
+  const [themeName, setThemeName] = useState<ThemeName>('light')
+  const currentTheme = themes[themeName];
+
   return (
-    <BrowserRouter>
-      <Header />
+    <ThemeProvider theme={currentTheme}>
+      <BrowserRouter>
+        <Header themeName={themeName} setThemeName={setThemeName} />
 
-      <Routes>
-        <Route path="/" element={<Profile />} />
-        <Route path="/:username" element={<Profile />} />
-        <Route path="/:username/:reponame" element={<Repo />} />
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Profile />} />
+          <Route path="/:username" element={<Profile />} />
+          <Route path="/:username/:reponame" element={<Repo />} />
+        </Routes>
 
-      <Footer />
+        <Footer />
 
-      <GlobalStyles />
-    </BrowserRouter>
+        <GlobalStyles />
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -12,7 +12,12 @@ export const GithubLogo = styled(FaGithub)`
   fill: var(--logo);
   width: 32px;
   height: 32px;
-  flex-shrink: 0; 
+  flex-shrink: 0;
+
+  cursor: pointer;
+  &:hover {
+    opacity: .7;
+  }
 `;
 
 export const  SearchForm = styled.form`
