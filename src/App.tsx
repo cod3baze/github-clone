eaa9864734ba52be79631b135fb725efaf53299d
eliasall3x <elliasallex@gmail.com>
2020-12-07 23:34:28 -0300

    feat: Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Footer from './components/Footer';
 
 // import { Container } from './styles';
 
@@ -20,7 +21,7 @@ const App: React.FC = () => {
         <Route path="/:username/:reponame" element={<Repo />} />
       </Routes>
 
-      {/* <Footer /> */}
+      <Footer />
 
       <GlobalStyles />
     </BrowserRouter>
