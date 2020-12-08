4e4620a20ed92ad8a8eba30ddabf68680989916b
eliasall3x <elliasallex@gmail.com>
2020-12-07 22:43:50 -0300

    feat: reponame styled

--- src/pages/Repo/styles.ts
+++ src/pages/Repo/styles.ts
@@ -13,7 +13,37 @@ export const Container = styled.div`
   }
 `;
 
-export const Breadcrumb =  styled.div``;
+export const Breadcrumb =  styled.div`
+  margin-bottom: 16px;
+
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap;
+  white-space: nowrap;
+
+  font-size: 18px;
+
+  > a {
+    color: var(--link);
+    text-decoration: none;
+
+    &:hover,
+    &:focus {
+      text-decoration: underline;
+    }
+
+    &.username {
+      margin-left: 8px;
+    }
+    &.reponame {
+      font-weight: 600;
+    }
+  }
+
+  > span {
+    padding: 0 5px;
+  }
+`;
 
 const iconCSS = css`
   width: 16px;
@@ -26,7 +56,22 @@ export const  RepoIcon =  styled(RiBookMarkLine)`
   ${iconCSS};
 `;
 
-export const  Stats =  styled.div``;
+export const  Stats =  styled.ul`
+  margin-top: 16px;
+  display: flex;
+  align-items: center;
+
+  > li {
+    display: flex;
+    align-items: center;
+    margin-right: 9px;
+
+    > * {
+      margin-right: 7px;
+      color: var(--gray);
+    }
+  }
+`;
 
 export const  StartIcon =  styled(RiStarLine)`
   ${iconCSS};
@@ -36,7 +81,26 @@ export const ForkIcon = styled(AiOutlineFork)`
   ${iconCSS};
 `;
 
-export const  LinkButton =  styled.a``;
+export const  LinkButton =  styled.a`
+  text-decoration: none;
+
+  margin-top: 24px;
+  background-color: var(--gray-dark);
+  padding: 12px 17px;
+  border-radius: 24px;
+  width: max-content;
+
+  display: flex;
+  align-items: center;
+
+  > span {
+    color: var(--primary);
+  }
+  > svg {
+    fill: var(--primary);
+    margin-right: 10px;
+  }
+`;
 
 export const  GithubIcon =  styled(FaGithub)`
   ${iconCSS};
